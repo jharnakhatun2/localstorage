d4e98fd7a2afb4d440cc0533a4bc550d2b3c48c7
jharnakhatun2 <jharnakhatun203@gmail.com>
2024-11-29 14:42:53 +0600

    create function for add product where catch user data

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("object");
+const addProduct = () => {
+const productName = document.getElementById('product-name');
+const productQuantity = document.getElementById('product-quantity');
+const productNameValue = productName.value.trim();
+const productQuantityValue = productQuantity.value.trim();
+// reset input
+productName.value = "";
+productQuantity.value = '';
+console.log(productNameValue, productQuantityValue);
+}
+
